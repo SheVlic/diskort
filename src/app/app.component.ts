7fe0bba100606d489724a59cef83b440068f3306
SheVlic <she.vl.adimir.ic@gmail.com>
2025-05-30 18:50:02 +0300

    добавил логику сообщений через ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,47 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'diskort';
-  XActiveButton = 'work1'; // текущая вкладка
+	title = 'diskort';
+	contacts = [
+		{name: 'Vova', avatar: "",lastMessage: "", online: true}, 
+		{name: 'Maks', avatar: "",lastMessage: "", online: true},
+		{name: 'Chmo Ebanoe', avatar: "",lastMessage: "", online: false},
+		{name: 'Chmo Ebanoe2', avatar: "",lastMessage: "", online: false}
+	];
+	messages = [
+		{userName: 'Vova', from: "me", text: "A", time: new Date()},
+		{userName: 'Maks', from: "other", text: "B", time: new Date()},
+		{userName: 'Vova', from: "other", text: "V", time: new Date()},
+		{userName: 'Chmo Ebanoe', from: "other", text: "G", time: new Date()},
+		{userName: 'Vova', from: "me", text: "D", time: new Date()}
+	]
+	activeContactName: string= this.contacts[0].name;
 
-  switchX(navButton: string) {
-    this.XActiveButton = navButton;
-  }
+	isActive(contactName: string): boolean {
+		return this.activeContactName === contactName;
+	}
+
+	setChat(contactName: string): void{
+		this.activeContactName = contactName;
+	}
+
+	getFilterMessages() {
+		return this.messages.filter((textMessage) => {
+			return this.activeContactName === textMessage.userName;
+		});
+
+	}
+
+	sendMessage(): void {
+		const inputElement = document.getElementById("message-input") as any;
+		const newMessage = {
+			userName: this.activeContactName, from: "me", text: inputElement.value, time: new Date()
+		};
+		this.messages.push(newMessage);
+	}
+
+
 }
+
 
 
