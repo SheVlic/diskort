1fb13eecb89f4781ee72eefc574f85d0456aeba0
SheVlic <she.vl.adimir.ic@gmail.com>
2025-06-07 18:26:46 +0300

    добавили сохранение информации в объект класса user

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { User } from '../model/user';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -14,6 +16,14 @@ export class RegisterComponent {
 		"Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
 	];
 	public years: number[] = [];
+	public nameControl: FormControl = new FormControl();
+	public emailControl: FormControl = new FormControl();
+	public nicknameControl: FormControl = new FormControl();
+	public passwordControl: FormControl = new FormControl();
+	public passwordConfirmControl: FormControl = new FormControl();
+	public birthDayControl: FormControl = new FormControl(1);
+	public birthMonthControl: FormControl = new FormControl("Январь");
+	public birthYearControl: FormControl = new FormControl(2000);
 
 	constructor() {
 		for (let i = 1; i <= 31; i++) {
@@ -27,7 +37,16 @@ export class RegisterComponent {
 	}
 
 	public register() :void {
+		const date: Date = new Date(this.birthYearControl.value, this.months.indexOf(this.birthMonthControl.value), this.birthDayControl.value);
+		const newUser: User = {
+			email: this.emailControl.value,
+			name: this.nameControl.value,
+			nickname: this.nicknameControl.value,
+			password: this.passwordControl.value,
+			birthDate: date
+		};
 
+		console.log(newUser);
 	}
 
 
